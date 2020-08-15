cf89222d63c64b16541f2b4f42f234711e6da313
Sergey Bolotnikov <sbolotnikov@gmail.com>
2020-08-15 01:38:38 -0400

    change port settings

--- server.js
+++ server.js
@@ -22,6 +22,9 @@ require("./routes/htmlRoutes")(app);
 
 // Starts the server to begin listening
 // =============================================================
-app.listen(PORT, function () {
-    console.log("App listening on PORT " + PORT);
-});
+// app.listen(PORT, function () {
+//     console.log("App listening on PORT " + PORT);
+// });
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
