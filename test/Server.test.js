d93496c5e48cb9d68672743cc3892cf436d2316a
Sergey Bolotnikov <sbolotnikov@gmail.com>
2020-08-17 00:18:10 -0400

    tests fixed

--- test/Server.test.js
+++ test/Server.test.js
@@ -2,39 +2,59 @@
 // var expect  = require('chai').expect;
 var request = require('request');
 var db = require("../db/db.json");
-const app = require("../server.js");
+const server = require("../server.js");
+const app = server.app;
 const chai = require("chai");
 const chaiHttp = require("chai-http");
-
+db = [];
 const { expect } = chai;
 chai.use(chaiHttp);
-describe("Server test:", () => {
+// console.log("before"+db);
+describe("Server test html routes", () => {
+  it(" Main page test", done => {
+    chai
+      .request(app).get("/")
+      .end((err, res, body) => {
+        expect(res.statusCode).to.equal(200);
+        expect(res.headers["content-type"]).to.equals("text/html; charset=UTF-8");
+        done();
+      });
+  });
+  it(" Edit note page test", done => {
+    chai
+      .request(app).get("/notes")
+      .end((err, res, body) => {
+        expect(res.statusCode).to.equal(200);
+        expect(res.headers["content-type"]).to.equals("text/html; charset=UTF-8");
+        done();
+      });
+  });
+});
+describe("Server test part one", () => {
 
   it(" POST testing", done => {
     chai
-      .request('http://localhost:3030').post("/api/notes").send({ title: "test1", text: "test1", id: 0 })
+      .request(app).post("/api/notes").send({ title: "test1", text: "test1"})
       .end((err, res) => {
         expect(res).to.have.status(200);
         expect(res.body).to.equals(true);
         done();
       });
   });
-  it(" DELETE testing", done => {
+  
+  it(" GET testing", done => {
     chai
-      .request('http://localhost:3030').delete("/api/notes/0")
+      .request(app).get("/api/notes")
       .end((err, res) => {
-        console.log(db)
         expect(res).to.have.status(200);
-        expect(res.body).to.equals('true');
+        expect(res.body).to.be.an('array');
         done();
       });
   });
-
   it(" PUT testing", done => {
     chai
-      .request('http://localhost:3030').put("/api/notes/0")
+      .request(app).put("/api/notes/0")
       .end((err, res) => {
-        console.log(db)
         expect(res).to.have.status(200);
         expect(res.body).to.equals('true');
         done();
@@ -42,66 +62,18 @@ describe("Server test:", () => {
   });
 });
 
-it('Main page test', function (done) {
-  request('http://localhost:3030', function (error, response, body) {
-    expect(response.statusCode).to.equal(200);
-    expect(response.headers["content-type"]).to.equals("text/html; charset=UTF-8");
-    expect(response.statusMessage).to.equals("OK");
-    done();
+describe("Server test part two", () => {
+  
+  it(" DELETE testing", done => {
+    chai
+      .request(app).delete("/api/notes/0")
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.body).to.equals('true');
+        done();
+      });
   });
 });
 
-it('Edit note page test ', function (done) {
-  request('http://localhost:3030/notes', function (error, response, body) {
-    expect(response.statusCode).to.equal(200);
-    expect(response.headers["content-type"]).to.equals("text/html; charset=UTF-8");
-    expect(response.statusMessage).to.equals("OK");
-    done();
-  });
-});
-it('API route test', function (done) {
-  request('http://localhost:3030/api/notes', function (error, response, body) {
-    expect(response.statusCode).to.equal(200);
-    expect(body).to.equals('[{"title":"test1","text":"test1","id":0}]');
-    done();
-  });
-});
-
-
-// function test(){
-// db=[];
-// db.push({title: "test1",text: "test1",id: 0});
-// }
-// test();
-// console.log(db)
-
-
-
-//   it("server starts edit notes page ", done => {
-//     chai
-//       .request(app)
-//       .get("/api/notes")
-//       .end((err, res) => {
-//         expect(res).to.have.status(200);
-//         expect(res.body.status).to.equals("success");
-//         expect(res.body.message).to.equals("Edit notes");
-//         done();
-//       });
-// it("adds 2 numbers", done => {
-//   chai
-//     .request(app)
-//     .post("/add")
-//     .send({ num1: 5, num2: 5 })
-//     .end((err, res) => {
-//       expect(res).to.have.status(200);
-//       expect(res.body.status).to.equals("success");
-//       expect(res.body.result).to.equals(10);
-//       done();
-//     });
-// });
-
-
-
-// });
 
 
