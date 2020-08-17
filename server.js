08870f9372e99c5011abca3c04cbd0faa5e8f122
Sergey Bolotnikov <sbolotnikov@gmail.com>
2020-08-17 00:59:48 -0400

    fixing port issue

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ require("./routes/htmlRoutes")(app);
 module.exports = {
   app: app,
   start: function(){
-    app.listen(process.env.PORT || 3030, function(){
+    app.listen(process.env.PORT || 3000, function(){
       console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
     });
   }
